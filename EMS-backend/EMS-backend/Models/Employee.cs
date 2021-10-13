b1f556dbab5fdcb755034f997eec81f899647faf
123JackCole <123jack.cole@gmail.com>
2021-10-13 13:54:01 -0700

    added more variables for employees

--- EMS-backend/EMS-backend/Models/Employee.cs
+++ EMS-backend/EMS-backend/Models/Employee.cs
@@ -9,7 +9,9 @@ namespace EMS_backend.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
 
         public string Email { get; set; }
 
@@ -18,6 +20,8 @@ namespace EMS_backend.Models
         public DateTime Birthday { get; set; }
 
         public string Address { get; set; }
+
+        public string Gender { get; set; }
 
         public string Role { get; set; }
     }
