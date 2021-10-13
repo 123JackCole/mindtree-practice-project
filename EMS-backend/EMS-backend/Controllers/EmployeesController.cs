938795815626e324af4cf21593c39d4265eb2efc
123JackCole <123jack.cole@gmail.com>
2021-10-13 13:47:34 -0700

    added in basic backend files

---
+++ EMS-backend/EMS-backend/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMS_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Employee list here!" };
+        }
+
+        //public ActionResult<IEnumerable<Employee>> GetAllEmployees()
+        //{
+        //    return employees;
+        //}
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "the value is " + id;
+        }
+
+    }
+}
